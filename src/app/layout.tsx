30bc5a8a5445882d5e3b6f128e8ff38d2aeee6eb
Brandon Slaa <branslaa@gmail.com>
2025-09-28 22:24:07 -0400

    googlle tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Titan_One, Quicksand } from 'next/font/google';
 import './globals.css';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const titanOne = Titan_One({
 	variable: '--font-titan-one',
@@ -29,6 +30,7 @@ export default function RootLayout({
 				className={`${titanOne.variable} ${quicksand.variable} font-sans antialiased`}
 			>
 				{children}
+				<GoogleTagManager gtmId="GTM-TSD5WPR4" />
 			</body>
 		</html>
 	);
